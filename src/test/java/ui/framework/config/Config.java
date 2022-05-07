61744ed72b9749dc536b9fb100c559414b1cca2f
edchancdn <edchan.cdn@gmail.com>
2022-05-07 14:50:28 -0400

    added config file

--- src/test/java/ui/framework/config/Config.java
+++ src/test/java/ui/framework/config/Config.java
@@ -1,4 +1,34 @@
 package ui.framework.config;
 
+import jdk.swing.interop.SwingInterOpUtils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Properties;
 public class Config {
+
+    private static Config instance;
+    private Properties properties;
+    private final String propertyFileName =  "framework.properties";
+
+    private Config(){
+        Path path = Paths.get("src", "test", "resources", propertyFileName);
+        try (InputStream stream = new FileInputStream(path.toFile())) {
+            properties = new Properties();
+            properties.load(stream);
+        }catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+
+    public synchronized static String getProperties(String key) {
+        if(instance == null) {
+            instance = new Config();
+        }
+        return instance.properties.getProperty(key);
+    }
 }
--- src/test/java/ui/framework/testcases/HomeTest.java
+++ src/test/java/ui/framework/testcases/HomeTest.java
@@ -1,4 +1,13 @@
 package ui.framework.testcases;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import ui.framework.config.Config;
+
 public class HomeTest {
+    @Test
+    public void testName() {
+
+        Assert.assertTrue( Config.getProperties("browser.name").equals("chrome"));
+    }
 }
