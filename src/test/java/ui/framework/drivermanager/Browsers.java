8e3c8520998aa301cae5fa37bc576031724d5561
Atin Singh <atin@pragra.co>
2022-05-13 10:24:47 -0400

    Added driver manager changes

--- src/test/java/ui/framework/drivermanager/Browsers.java
+++ src/test/java/ui/framework/drivermanager/Browsers.java
@@ -1,2 +1,8 @@
-package ui.framework.drivermanager;public class Broswers {
+package ui.framework.drivermanager;
+
+public class Browsers {
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String IE = "ie";
+    public static final String SAFARI = "safari";
 }
