4f6cf3bfd308b2aca6c0927d5d7457d7f0672a8b
Atin Singh <atin@pragra.co>
2022-05-14 14:40:43 -0400

    updated code fro wait

---
+++ src/test/java/ui/framework/testcases/MyConditions.java
@@ -0,0 +1,11 @@
+package ui.framework.testcases;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+
+public class MyConditions implements ExpectedCondition<WebDriver> {
+    @Override
+    public WebDriver apply(WebDriver webDriver) {
+        return null;
+    }
+}
